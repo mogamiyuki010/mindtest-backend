7b008225dcfd2bf7ce693a1fb2bfd99d2836ec52
mogamiyuki010 <heresy0416@gmail.com>
2025-10-16 23:00:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -223,6 +223,6 @@ app.get("/api/realtime", (req, res) => {
 
 // === 啟動伺服器 ===
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0" () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
